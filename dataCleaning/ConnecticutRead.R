76acf907b3cd5e2436452036866aff9ce569cba1
Lief <williamlief@gmail.com>
2019-12-02 23:28:30 -0800

    removed old ct code, opted for a copy paste job

--- dataCleaning/ConnecticutRead.R
+++ dataCleaning/ConnecticutRead.R
@@ -1,16 +1,7 @@
-url <- "https://projects.ctmirror.org/tools/fancytable/table.html?d=Schooldistrictevaluationsbetween2013and2015-3-1-2016-31188"
+# Setup ------------------------------------------------------------------------
 
-X <- rvest()
 
-library(rvest)
-library(magrittr)
+# Read -------------------------------------------------------------------------
 
-url %>%
-  read_html() %>%
-  html_nodes("iframe") %>%
-  extract(3) %>% 
-  html_attr("src") %>% 
-  read_html() %>% 
-  html_node("#searchResultsTable") %>% 
-  html_table() %>%
-  head()
+
+# Clean ------------------------------------------------------------------------
