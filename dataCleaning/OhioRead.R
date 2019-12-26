1bffcb27d1cbcdddb0f0adc45ec868158f465c72
Lief Esbenshade <williamlief@gmail.com>
2019-12-26 06:52:26 -0800

    Cleaning data (#31)
    * Cleaned Ohio district data from 2017, 2018, and 2019.
    * Added Eric's cleaning code into OhioRead.R, had to rework a fair bit
    * removed ohioUpdate script
    * Updates to compilation script.
    - Better flagging for data that has p's but not e's
    - filling NA e's with 0
    - quality checking code (will be removed later)
    
    Co-authored-by: ezhang6811 <eric.zhang6811@gmail.com>

--- dataCleaning/OhioRead.R
+++ dataCleaning/OhioRead.R
@@ -7,6 +7,7 @@ library(readxl)
 source("setup.r")
 
 path <- setpath("Ohio")
+path2 <- stringr::str_replace(path, "Ohio/evaluation", "Ohio/District_teacher_data")
 
 # Read 2013-14 data ------------------------------------------------------------
 
@@ -45,14 +46,83 @@ Ohio_14 <- df %>%
          
          et = e1+e2+e3+e4,
          year = 2014) %>% 
-  mutate_if(is.numeric, as.integer) %>% 
   select(state, year, localid, name, e1, e2, e3, e4, et, e1_impute, e2_impute, e3_impute, e4_impute)
 
 # Read District_teacher_data ---------------------------------------------------
 
+# 2017 data
+df1 <- read_excel(paste(path2, 
+                        "DISTRICT_TEACHER_2017_fin.xls", 
+                        sep = "/"), 
+                  sheet=1) %>% 
+  select(localid = `District IRN`,
+         name = `District Name`,
+         et = `Average Number of Full Time Teachers`,
+         p1 = `% of Teachers  Evaluated as  Ineffective`,
+         p2 = `% of Teachers  Evaluated as  Developing`,
+         p3 = `% of Teachers  Evaluated as Skilled`,
+         p4 = `% of Teachers  Evaluated as  Accomplished`,
+         ps = `% of Teachers  Evaluations Not Completed`) %>% 
+  mutate(year = 2017, 
+         p1=as.numeric(p1) * 100,
+         p2=as.numeric(p2) * 100,
+         p3=as.numeric(p3) * 100,
+         p4=as.numeric(p4) * 100)
 
+# 2018 data
+df2 <- read_excel(paste(path2, "DIST_LRC_2018_EDUCATOR_DATA.xlsx",
+                        sep = "/"),
+                  sheet = 1) %>% 
+  select(localid = `District IRN`,
+         name = `District Name`,
+         et = `FTE Full Time Tchrs`,
+         p1 = `Pct Tchrs Evaluated Ineffective`,
+         p2 = `Pct Tchrs Evaluated Developing`,
+         p3 = `Pct Tchrs Evaluated Skilled`,
+         p4 = `Pct Tchrs Evaluated Accomplished`,
+         ps = `Pct Tchrs Eval Not Completed`) %>% 
+  mutate(year = 2018)
 
+# 2019 data
+df3 <- read_excel(paste(path2, "DIST_LRC_2019_EDUCATOR_DATA.xlsx",
+                        sep = "/"),
+                  sheet = 2) %>%
+  select(localid = `District IRN`,
+         name = `District Name`,
+         et = `Number of Full Time Teachers (FTE)`,
+         p1 = `Percent of Teachers Evaluated as Ineffective`,
+         p2 = `Percent of Teachers Evaluated as Developing`,
+         p3 = `Percent of Teachers Evaluated as Skilled`,
+         p4 = `Percent of Teachers Evaluated as Accomplished`,
+         ps = `Percent Teachers whose Evaluations were Not Completed`) %>% 
+  mutate(year = 2019)
 
+Ohio_dtd <- bind_rows(df1, df2, df3) %>% 
+  mutate(name = tolower(name),
+         state = "OH") %>% 
+  mutate(et = as.numeric(et))
 
-  
+# Combine and save -------------------------------------------------------------
+
+Ohio <-
+  bind_rows(Ohio_14, Ohio_dtd) %>% 
+  arrange(localid, year)
+
+# checking n obs by years observed
+test <- Ohio %>% group_by(localid) %>% mutate(years = paste(year, collapse = ",")) %>% ungroup()
+test %>% 
+  group_by(years) %>% 
+  summarize(
+    n = n(),
+    sum(et))
+check1 <- test %>% filter(years == "2014")
+check2 <- test %>% filter(years == "2017,2018,2019") %>% distinct(localid, name)
+# The 2014 only seem to be all charter schools
+# The 2014 document states that not all district implemented evaluations. 
+# We see 'defiance city' has evaluated principals but not teachers in 2014, has
+# evaluation data for teachers in 2017+. 
+# leaving data as is - will need to be careful when comparing trends from 2014
+# to 2017+ due to shift in districts represented. BUT already need to be cautious
+# because of time gap. Approximately 82% of teacher count is present in all years.
+
 readr::write_csv(Ohio, "CleanData/OhioEval.csv")
--- dataCleaning/compile.R
+++ dataCleaning/compile.R
@@ -5,6 +5,7 @@
 # Setup ------------------------------------------------------------------------
 
 library(dplyr)
+library(tidyr)
 library(readr)
 
 # Read in Files ----------------------------------------------------------------
@@ -20,21 +21,37 @@ df <- files %>% bind_rows()
 
 # Create p_ variables ----------------------------------------------------------
 
-df2 <- df %>% 
+df <- df %>% 
   # LA data only has percents and no counts, need to exclude from this calculation
-  filter(state != "LA") %>% 
+  # Ohio 2017-19 data is also percent only
+  mutate(has_p = 
+           state == "LA" |
+           (state == "OH" & year %in% 2017:2019)
+  )
+
+df2 <- df %>% 
+   filter(!has_p) %>% 
+  replace_na(list(e1 = 0, e2 = 0, e3 = 0, e4 = 0)) %>% 
   mutate(
     p1 = e1 / et, 
     p2 = e2 / et, 
     p3 = e3 / et, 
     p4 = e4 / et,
     ps = es / et, 
-    pu = eu / et
+    pu = eu / et,
+    pt = p1+p2+p3+p4+p2+pu
   ) %>% 
   bind_rows(df %>% 
-              filter(state == "LA") %>% 
+              filter(has_p) %>% 
               mutate_at(vars(p1, p2, p3, p4), ~. / 100))
 
+df2 %>% group_by(state) %>% summarize(sum(is.na(et)))
+# check p data not missing
+df2 %>% group_by(state) %>% summarize(sum(is.na(p1)))
+df2 %>% group_by(state) %>% summarize(sum(is.na(p2)))
+df2 %>% group_by(state) %>% summarize(sum(is.na(p3)))
+df2 %>% group_by(state) %>% summarize(sum(is.na(p4)))
+df2 %>% mutate(pt = p1+p2+p3+p4) %>% group_by(state) %>% summarize(mean(is.na(pt)))
 
 # Merge NCES -------------------------------------------------------------------
 
@@ -49,7 +66,8 @@ df_nces <- df2 %>%
       state == "MI" ~ stringr::str_pad(localid, 5, side = "left", "0"),
       TRUE ~ localid
     )) %>% 
-  left_join(nces, by = c("state", "year", "localid"))
+  left_join(nces, by = c("state", "year", "localid")) %>% 
+  filter(!(state == "ID" & is.na(NCES_leaid))) # visual check confirmed these are all charter schools
 
 df_nces %>% group_by(state) %>% summarize(sum(is.na(NCES_leaid)))
 
@@ -80,4 +98,4 @@ evaluationData <- df_nces %>%
     "impute_level4" = e4_impute
   )
 
-saveRDS(df, "cleanData/evaluationData.rds")
+saveRDS(evaluationData, "cleanData/evaluationData.rds")
