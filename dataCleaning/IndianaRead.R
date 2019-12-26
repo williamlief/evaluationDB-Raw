775227defece5aeb78ac80d03d0f67c518003e5b
Lief <williamlief@gmail.com>
2019-12-26 08:59:40 -0800

    Got new Indiana data and rewrote code fix #21 fix #20

--- dataCleaning/IndianaRead.R
+++ dataCleaning/IndianaRead.R
@@ -11,71 +11,138 @@ indiana <- substring(path, 1, nchar(path) - nchar("evaluation"))
 
 # Read -------------------------------------------------------------------------
 
-corp.y1 <- read_excel(
-  paste(path, "sboe-data-er-data-report-12-13.xlsx", sep = "/"),
-  sheet= "12-13 corp level ER data",
-  skip=1)
+data <- list()
 
-corp.y2 <- read_excel(
-  paste(path, "sboeresultsdatareport2013-14.xlsx", sep = "/"),
-  sheet= "Corp",
-  range=cell_cols("A:I"))
+data[[1]] <- 
+  read_excel(
+    paste(path, "sboe-data-er-data-report-12-13.xlsx", sep = "/"),
+    sheet= "12-13 corp level ER data",
+    skip=1, 
+    col_types = "text") %>% 
+  rename(
+    district = `CORP #`, 
+    name = `CORPORATION NAME`, 
+    e1 = INEFFECTIVE,
+    e2 = `IMPROVEMENT NECESSARY`, 
+    e3 = `EFFECTIVE`, 
+    e4 = `HIGHLY EFFECTIVE`, 
+    eu = `NA`, 
+    et = `TOTAL EDUCATORS REPORTED`
+  ) %>% 
+  mutate(year = 2013)
 
-corp.y3 <- read_excel(
-  paste(path, "2014-15-evaluation-ratings-er-data-website.xlsx", sep = "/"),
-  sheet= "Corporation")
+data[[2]] <- 
+  read_excel(
+    paste(path, "sboeresultsdatareport2013-14.xlsx", sep = "/"),
+    sheet= "Corp",
+    range=cell_cols("A:I"), 
+    col_types = "text") %>% 
+  rename(
+    district = `Corp`, 
+    name = `Corp Name`, 
+    e1 = `1-Ineffective`,
+    e2 = `2-Needs Improvement`, 
+    e3 = `3-Effective`, 
+    e4 = `4-Highly Effective`, 
+    eu = `0-N/A`, 
+    et = `Total`
+  ) %>% 
+  mutate(year = 2014) %>% 
+  select(-`Corp Grade`) %>% 
+  filter(!is.na(name))
 
-corp.y4 <- read_excel(
-  paste(path, "2015-16evaluationratings-er-data.xlsx", sep = "/"),
-  "Corporation")
+data[[3]] <- 
+  read_excel(
+    paste(path, "2014-15-evaluation-ratings-er-data-website.xlsx", sep = "/"),
+    sheet= "Corporation", 
+    col_types = "text") %>% 
+  rename(
+    name = `CorpName`, 
+    e1 = `Ineffective\r\n(1)`  ,
+    e2 = `Improvement \r\nNecessary\r\n(2)`, 
+    e3 = `Effective \r\n(3)`, 
+    e4 = `Highly \r\nEffective\r\n(4)`, 
+    eu = `Not Rated`, 
+    et = `Grand Total`
+  ) %>% 
+  mutate(year = 2015)
 
-# most recent district directory
-directory <- read_excel(paste0(indiana,"/2017-2018-school-directory-2017-08-07.xlsx")) %>% 
-    select(corpnum=1,corp=2) %>% 
-    mutate(corpnum=as.numeric(corpnum)) %>% 
-    arrange(corp)
+data[[4]] <- 
+  read_excel(
+    paste(path, "2015-16evaluationratings-er-data.xlsx", sep = "/"),
+    sheet = "Corporation", 
+    col_types = "text") %>% 
+  rename(
+    name = `Corporation Name`, 
+    e1 = `Ineffective \r\n(1)`  ,
+    e2 = `Improvement Necessary \r\n(2)`, 
+    e3 = `Effective \r\n(3)`, 
+    e4 = `Highly Effective \r\n(4)`, 
+    eu = `Not Rated`, 
+    et = `Grand Total`
+  ) %>% 
+  mutate(year = 2016)
 
-# stack up the data
-# corp
-names(corp.y1) <- c("corpnum", "corp", "e4", "e3", "e2", "e1", "unrated", "total")
-corp.y1$year = 2013
-corp.y1$e4 <- as.numeric(corp.y1$e4)
+# Data format becomes more consistent
+er_report_read <- function(filename, sheet = "Corporation") {
+  read_excel(
+    path = paste(path, filename, sep = "/"), 
+    sheet = sheet, 
+    col_types = "text",
+    skip = 1
+  ) %>% 
+    select(
+      name = `Corporation Name`, 
+      e1 = Ineffective,
+      e2 = `Needs Improvement`, 
+      e3 = `Effective`, 
+      e4 = `Highly Effective`,
+      eu = `Not Rated`,
+      et = `Grand Total`
+    )
+}
 
-names(corp.y2) <- c("corpnum", "corp", "corpgrade", "unrated", "e1", "e2", "e3", "e4", "total")
-corp.y2$corpnum <- as.numeric(corp.y2$corpnum)
-corp.y2$unrated<- as.numeric(corp.y2$unrated)
-corp.y2$year = 2014
+data[[5]] <- 
+  er_report_read("er-report-final.xlsx") %>% 
+  mutate(year = 2017)
 
-names(corp.y3) <- c("corp", "e1", "e2", "e3", "e4","unrated", "total")
-corp.y3$e1 <- as.numeric(corp.y3$e1)
-corp.y3$year = 2015
+data[[6]] <- 
+ er_report_read("17-18-er-data.xlsx") %>% 
+  mutate(year = 2018)
 
-names(corp.y4) <- c("corp", "e1", "e2", "e3", "e4","unrated", "total")
-corp.y4$year = 2016
+data[[7]] <- 
+  er_report_read("er-report-18-19-sboe.xlsx", sheet = "Corportation") %>% 
+  mutate(year = 2019)
 
-corp <- bind_rows(corp.y1, corp.y2, corp.y3, corp.y4) %>% 
-    select(-corpgrade) %>% 
-    left_join(directory, by="corp") %>% 
-    mutate(state="IN",
-           corpnum=coalesce(corpnum.x,corpnum.y)) %>% 
-    select(-corpnum.x, -corpnum.y) %>% 
-    mutate(corp = tolower(corp),
-           corp = gsub("community", "com", corp),
-           corp = gsub("corporation", "corp", corp),
-           corp = gsub("district", "dist", corp),
-           corp = gsub("school", "sch", corp)) %>% 
-    arrange(corp, year) %>% 
-    mutate(corpnum = ifelse(corp==lag(corp) & year==2015, lag(corpnum),corpnum),
-           corpnum = ifelse(corp==lag(corp) & year==2016, lag(corpnum),corpnum),
-           corpnum = as.character(corpnum)) %>% 
-    mutate(e1 = ifelse(is.na(e1),0,e1),
-           e2 = ifelse(is.na(e2),0,e2),
-           e3 = ifelse(is.na(e3),0,e3),
-           e4 = ifelse(is.na(e4),0,e4),
-           unrated = ifelse(is.na(unrated),0,unrated),
-           total = ifelse(is.na(total),0,total)) %>% 
-  mutate_if(is.numeric, as.integer) %>% 
-  filter(!is.na(corp)&!is.na(corpnum)) %>% 
-  select(state, year, localid=corpnum, name=corp, e1, e2, e3, e4, eu=unrated, et=total)
+# district directory to merge on district numbers
+directory <- read_excel(paste0(indiana,"/2017-2018-school-directory-2017-08-07.xlsx"),
+                        col_types = 'text') %>% 
+    select(district=`IDOE CORPORATION ID`,
+           name = `CORPORATION NAME`) %>% 
+    arrange(district)
 
-write_csv(corp, "cleanData/IndianaEval.csv")
+# stack up the data, fill in nces ids
+df <- bind_rows(data) %>% 
+  filter(!name %in% c("Grand Total", "(blank)")) %>% 
+  left_join(directory, by = "name") %>% 
+  mutate(state="IN",
+         district =coalesce(district.x, district.y)) %>% 
+  select(-district.x, -district.y) %>% 
+  arrange(name, year) %>% 
+  mutate(district = if_else(is.na(district) & name == lag(name), lag(district), district))
+
+test <- df %>% filter(is.na(district))
+
+# convert numeric
+na_conv <- function(x) {
+  x = ifelse(tolower(x) == 'less than 10 educators reported', NA, x)
+  as.numeric(x)
+}
+
+df2 <- df %>% 
+  mutate_at(
+    vars(e1, e2, e3, e4, eu, et),
+    na_conv
+  )
+
+write_csv(df2, "cleanData/IndianaEval.csv")
