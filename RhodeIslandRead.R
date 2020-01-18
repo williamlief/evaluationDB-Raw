08efd1e387bf9858a9f91abd78597349215d6b20
ezhang6811 <eric.zhang6811@gmail.com>
2020-01-18 13:23:14 -0800

    RI script

---
+++ RhodeIslandRead.R
@@ -0,0 +1,27 @@
+source("setup.r")
+
+path <- setpath("Rhode Island")
+
+library(dplyr)
+library(readxl)
+library(tidyr)
+library(readr)
+library(stringr)
+
+df <- read_excel(paste(path, 
+                        "numbers of educators by YR-LEA-FERating-ToSend.xlsx", 
+                        sep = "/"), 
+                  sheet=1) %>% 
+  rename(name = `Row Labels`,
+         e4 = `HE`,
+         e3 = `E`,
+         e2 = `D`,
+         e1 = `I`,
+         es = `Not available`,
+         et = `Grand Total`)
+
+RhodeIsland <- df %>% 
+  mutate(year = as.numeric(substr(name, 1, 4)),
+         localid = str_split(name, "\\|")[2])
+
+         
