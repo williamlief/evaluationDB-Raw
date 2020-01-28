670c519c4fe1c47faeac2f67945600593a91d309
Lief <williamlief@gmail.com>
2020-01-27 23:17:06 -0800

    added localids to connecticut data fixed one misspelled district name

--- data-raw/scripts/ConnecticutRead.R
+++ data-raw/scripts/ConnecticutRead.R
@@ -9,28 +9,29 @@ path <- "data-raw/Connecticut/evaluation"
 
 # Read -------------------------------------------------------------------------
 
-df <- read_excel(paste0(path, "/ctmirror_copypaste.xlsx"), 
-                       sheet = 1, skip = 1, na = "") %>% 
+df <- read_excel(paste0(path, "/ctmirror_copypaste.xlsx"),
+                       sheet = 1, skip = 1, na = "") %>%
   rename(districtyear = `District`,
          e1 = `Below Standard`,
          e2 = `Developing`,
          e3 = `Proficient`,
          e4 = `Exemplary`,
          es = `Unaccounted`,
-         et = `Staff`) %>% 
+         et = `Staff`) %>%
   separate(districtyear, c('name', 'year'), sep = ' \\(')
 
 # Clean ------------------------------------------------------------------------
 
 localid <- read_csv("data-raw/scripts/CT_name_localid_xwalk.csv")
 
-CT <- df %>% 
+CT <- df %>%
   mutate(name = tolower(name),
+         name = if_else(name == "brakhamsted", "barkhamsted", name),
          state = "CT",
          year = as.numeric(substr(year, 1, 4)) + 1
-  ) %>% 
-  left_join(localid, by = "name") %>% 
+  ) %>%
+  left_join(localid, by = "name") %>%
   select(state, name, localid, year, e1, e2, e3, e4, es, et)
-  
+
 
 write_csv(CT, "data-raw/clean_csv_files/ConnecticutEval.csv")
